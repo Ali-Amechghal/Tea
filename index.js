9c53b060990c5c15cf42e0616e92f70d1e733ab2
Ali-Amechghal <ali.amechghal@gmail.com>
2016-08-31 21:18:28 +0100

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+var ansi = require('ansi-styles');
+var tea=(function(root){
+
+	var tea={};
+	tea.setBackground=function(text ,color){
+
+	};
+	tea.setColor= function(text , color){
+
+	};
+
+	return tea;
+}(this));
+
+module.exports = tea;
